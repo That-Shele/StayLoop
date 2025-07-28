83f9b88afb6bf123101102deb3b411f5bfe882ca
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-07-28 08:18:28 -0600

    Inicialización del proyecto

---
+++ src/main/java/org/esfe/stayloop/StayLoopApplication.java
@@ -0,0 +1,13 @@
+package org.esfe.stayloop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StayLoopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StayLoopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/esfe/stayloop/StayLoopApplicationTests.java
@@ -0,0 +1,13 @@
+package org.esfe.stayloop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayLoopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
