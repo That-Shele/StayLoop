a714054b0b243b79038ed676be1bf474f235f360
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-08-11 10:06:28 -0600

    Pequeño cambio en Repos

--- src/main/java/org/esfe/stayloop/repositorios/IHotelRepository.java
+++ src/main/java/org/esfe/stayloop/repositorios/IHotelRepository.java
@@ -6,7 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IHotelRepository extends JpaRepository<Hotel, Integer> {
-    List<Hotel> findByNombreContaining(String nombre);
+    List<Hotel> findByNombreContainingIgnoreCases(String nombre);
 
     List<Hotel> findByIdZona(Integer idZona);
 }
--- src/main/java/org/esfe/stayloop/repositorios/IRolRepository.java
+++ src/main/java/org/esfe/stayloop/repositorios/IRolRepository.java
@@ -6,5 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
 public interface IRolRepository extends JpaRepository<Rol, Integer> {
-    List<Rol> findByNombreContaining(String nombre);
+    List<Rol> findByNombreContainingIgnoreCase(String nombre);
 }
