fb7326156499b7de192283ccc22b6afd8573e6bd
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-08-11 08:12:38 -0600

    Se modificó repositorio de reserva

--- src/main/java/org/esfe/stayloop/repositorios/IReservaRepository.java
+++ src/main/java/org/esfe/stayloop/repositorios/IReservaRepository.java
@@ -9,6 +9,9 @@ import java.util.List;
 
 public interface IReservaRepository extends JpaRepository<Reserva, Integer> {
     List<Reserva> findByFechaFinBefore(LocalDateTime fechaFin);
+    List<Reserva> findByFechaInicioBefore(LocalDateTime fechaInicio);
+    List<Reserva> findByFechaFinAfter(LocalDateTime fechaFin);
+    List<Reserva> findByFechaInicioAfter(LocalDateTime fechaInicio);
     List<Reserva> findByIdUsuario(Integer idUsuario);
     List<Reserva> findByTotalGreaterThanEqual(BigDecimal total);
     List<Reserva> findByIdUsuarioAndIdHotel(Integer idUsuario, Integer idHotel);
