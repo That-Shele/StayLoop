b709c88253d458734a728f0431335a5c931013eb
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-08-13 00:26:42 -0600

    Formulario registrarse creado

--- src/main/java/org/esfe/stayloop/controladores/HomeController.java
+++ src/main/java/org/esfe/stayloop/controladores/HomeController.java
@@ -1,5 +1,8 @@
 package org.esfe.stayloop.controladores;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.esfe.stayloop.modelos.Usuario;
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,5 +14,22 @@ public class HomeController {
     @GetMapping
     public String index(){
         return "home/index";
+    }
+
+    @GetMapping("/login" )
+    public String mostrarLogin() {
+        return "home/formLogin";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, null, null);
+        return "redirect:/";
+    }
+
+    @GetMapping("/register")
+    public String mostrarRegistrarse(Usuario usuario){
+        return "home/formRegister";
     }
 }
