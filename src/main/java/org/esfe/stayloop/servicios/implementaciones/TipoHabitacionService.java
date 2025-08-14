b7ab4adfb11a7d0f3c45a7d5380fb444ba5e09f7
Walter Ivan Rosa Sandoval <ivanrosasandoval@gmail.com>
2025-08-14 15:11:32 -0600

    Interface e Implementacion de  TipoHabitacion fueron Creadas por(Ivan Rosa)

--- src/main/java/org/esfe/stayloop/servicios/implementaciones/TipoHabitacionService.java
+++ src/main/java/org/esfe/stayloop/servicios/implementaciones/TipoHabitacionService.java
@@ -10,45 +10,45 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.List;
 
-@Service
-public class TipoHabitacionService  implements ITipoHabitacionService {
+ @Service
+ public class TipoHabitacionService  implements ITipoHabitacionService {
 
-    @Autowired
-    private ITipoHabitacionRepository tipoHabitacionRepository;
+     @Autowired
+     private ITipoHabitacionRepository tipoHabitacionRepository;
 
-    @Override
-    public  List<TipoHabitacion> obtenerTodos() {
-        return tipoHabitacionRepository.findAll();
-    }
+     @Override
+     public  List<TipoHabitacion> obtenerTodos() {
+         return  tipoHabitacionRepository.findAll();
+     }
 
-    @Override
-    public  List<TipoHabitacion> buscarPorCantidadHabitacion(byte cantHab) {
-        return tipoHabitacionRepository.findByCantHabGreaterThanEqual(cantHab);
-    }
+     @Override
+     public  List<TipoHabitacion> buscarPorCantidadHabitacion(byte cantHab) {
+         return  tipoHabitacionRepository.findByCantHabGreaterThanEqual(cantHab);
+     }
 
-    @Override
-    public  List<TipoHabitacion> buscarPorCantidadPersona (byte cantPersonas) {
-        return tipoHabitacionRepository.findByCantPersonasGreaterThanEqual(cantPersonas);
-    }
+     @Override
+     public  List<TipoHabitacion> buscarPorCantidadPersona (byte cantPersonas) {
+         return  tipoHabitacionRepository.findByCantPersonasGreaterThanEqual(cantPersonas);
+     }
 
-    @Override
-    public  List<TipoHabitacion> buscarPorCosto (BigDecimal costo) {
-        return tipoHabitacionRepository.findByCostoGreaterThan(costo);
-    }
+     @Override
+     public  List<TipoHabitacion> buscarPorCosto (BigDecimal costo) {
+         return  tipoHabitacionRepository.findByCostoGreaterThan(costo);
+     }
 
 
-    @Override
-    public  TipoHabitacion buscarPorId(Integer id) {
-        return tipoHabitacionRepository.findById(id).get();
-    }
+     @Override
+     public  TipoHabitacion buscarPorId(Integer id) {
+         return  tipoHabitacionRepository.findById(id).get();
+     }
 
-    @Override
-    public   TipoHabitacion crearOEditar (TipoHabitacion tipoHabitacion) {
-        return tipoHabitacionRepository.save(tipoHabitacion);
-    }
+     @Override
+     public   TipoHabitacion crearOEditar (TipoHabitacion tipoHabitacion) {
+         return  tipoHabitacionRepository.save(tipoHabitacion);
+     }
 
-    @Override
-    public void  eliminarPorId (Integer id) {
-        tipoHabitacionRepository.deleteById(id);
-    }
+     @Override
+     public void  eliminarPorId (Integer id) {
+          tipoHabitacionRepository.deleteById(id);
+     }
 }
--- src/main/java/org/esfe/stayloop/servicios/interfaces/ITipoHabitacionService.java
+++ src/main/java/org/esfe/stayloop/servicios/interfaces/ITipoHabitacionService.java
@@ -1,21 +1,21 @@
-package org.esfe.stayloop.servicios.interfaces;
+ package org.esfe.stayloop.servicios.interfaces;
 
-import org.esfe.stayloop.modelos.TipoHabitacion;
+ import org.esfe.stayloop.modelos.TipoHabitacion;
 
 
-import java.math.BigDecimal;
-import java.util.List;
+ import java.math.BigDecimal;
+ import java.util.List;
 
-public interface  ITipoHabitacionService  {
+ public interface  ITipoHabitacionService  {
     List<TipoHabitacion> obtenerTodos();
 
-    List<TipoHabitacion>  buscarPorCantidadHabitacion (byte cantHab);
-    List<TipoHabitacion>  buscarPorCantidadPersona (byte cantPersonas);
-    List<TipoHabitacion>  buscarPorCosto (BigDecimal costo);
+    List<TipoHabitacion>  buscarPorCantidadHabitacion  (byte cantHab);
+    List<TipoHabitacion>  buscarPorCantidadPersona  (byte cantPersonas);
+    List<TipoHabitacion>  buscarPorCosto  (BigDecimal costo);
 
     TipoHabitacion  buscarPorId (Integer id);
 
-    TipoHabitacion  crearOEditar (TipoHabitacion tipoHabitacion);
+    TipoHabitacion crearOEditar (TipoHabitacion tipoHabitacion);
 
-    void eliminarPorId  (Integer id);
+    void eliminarPorId (Integer id);
 }
