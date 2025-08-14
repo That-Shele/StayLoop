4eafa2b85862406af0c189431d88c474c560bc4b
Walter Ivan Rosa Sandoval <ivanrosasandoval@gmail.com>
2025-08-14 16:13:32 -0600

    Interface e Implementacion de Zona fueron Creadas

--- src/main/java/org/esfe/stayloop/servicios/implementaciones/ZonaService.java
+++ src/main/java/org/esfe/stayloop/servicios/implementaciones/ZonaService.java
@@ -1,4 +1,44 @@
 package org.esfe.stayloop.servicios.implementaciones;
 
-public class ZonaService {
+import org.esfe.stayloop.modelos.Zona;
+import org.esfe.stayloop.repositorios.IZonaRepository;
+import org.esfe.stayloop.servicios.interfaces.IZonaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ZonaService implements IZonaService {
+
+    @Autowired
+    private IZonaRepository zonaRepository;
+
+
+    @Override
+    public List<Zona> obtenerTodos() {
+        return zonaRepository.findAll();
+    }
+
+    @Override
+    public Page<Zona> buscarPaginados(Pageable pageable, Integer zona) {
+        return zonaRepository.findByNombreContainingIgnoreCaseOrderByIdDesc(zona, pageable);
+    }
+
+    @Override
+    public Zona buscarPorId(Integer id) {
+        return zonaRepository.findById(id).get();
+    }
+
+    @Override
+    public Zona crearOEditar(Zona zona) {
+        return zonaRepository.save(zona);
+    }
+
+    @Override
+    public void eliminarPorId(Integer id) {
+          zonaRepository.deleteById(id);
+    }
 }
--- src/main/java/org/esfe/stayloop/servicios/interfaces/IZonaService.java
+++ src/main/java/org/esfe/stayloop/servicios/interfaces/IZonaService.java
@@ -1,4 +1,19 @@
 package org.esfe.stayloop.servicios.interfaces;
 
+import org.esfe.stayloop.modelos.Zona;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
 public interface IZonaService {
+    List<Zona> obtenerTodos();
+
+    Page<Zona> buscarPaginados(Pageable pageable, Integer zona);
+
+    Zona buscarPorId(Integer id);
+
+    Zona crearOEditar(Zona zona);
+
+    void eliminarPorId(Integer id);
 }
