eaf1c3124d51819180f0e7e50681dce458f6a176
Maynor Alejandro Bolaños Orellana <1925597@clases.edu.sv>
2025-08-14 10:55:58 -0600

    Interfaz e implementación de usuario completada

---
+++ src/main/java/org/esfe/stayloop/servicios/interfaces/IUsuarioService.java
@@ -0,0 +1,20 @@
+package org.esfe.stayloop.servicios.interfaces;
+
+import org.esfe.stayloop.modelos.Hotel;
+import org.esfe.stayloop.modelos.Usuario;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+
+import java.util.List;
+
+public interface IUsuarioService {
+    List<Usuario> obtenerTodos();
+
+    Page<Usuario> buscarPaginados(Pageable pageable, Integer idRol, String nombre, String email);
+
+    Usuario buscarPorId(Integer id);
+
+    Usuario crearOEditar(Usuario usuario);
+
+    void eliminarPorId(Integer id);
+}
